e31da3ddeab53717d0c3ef4ea40d0caf1126d4a8
luka dodelia <luka@picktek.com>
2022-01-17 22:01:45 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,1 @@
+console.log('run test.js to see how it stuck run test2.js for working sample')
---
+++ test.js
@@ -0,0 +1,14 @@
+import fs              from 'fs';
+import { PDFDocument } from 'pdf-lib';
+
+const pdfDoc = await PDFDocument.create();
+pdfDoc.addPage();
+const page = pdfDoc.getPage(0);
+
+const testpng = await pdfDoc.embedPng(fs.readFileSync(`test.png`));
+testpng.scaleToFit(150, 30);
+
+page.drawImage(testpng, {
+  x: 10, y: 10, width: 150, height: 30
+});
+fs.writeFileSync('output.pdf', await pdfDoc.save());
